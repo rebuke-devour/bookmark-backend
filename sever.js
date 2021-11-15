946f7cda12abde015cbe148742522027643a3177
John Torres <rebuke.devour@gmail.com>
2021-11-15 15:12:53 -0500

    Bookmark routing Complete

--- sever.js
+++ sever.js
@@ -37,13 +37,69 @@ app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
 
-
 //////////////////////
 // Routes
 //////////////////////
+// create a test route
+app.get("/", (req, res) => {
+    res.send("hello world");
+  });
 
+//Index Bookmarks
+app.get('/bookmark', async(req, res)=>{
+    try {
+        res.json(await Bookmark.find({}));
+    } catch (error){
+        res.status(400).json(error);
+    }
+});
 
+//Create Bookmarks
+app.put('/bookmark', async (req, res)=>{
+    try {
+        res.json(await Bookmark.create(req.body));
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
 
+//Update Bookmarks
+app.put('/bookmark/:id', async (req, res)=>{
+    try {
+        res.json(
+            await Bookmark.findByIdAndUpdate(req.params.id, req.body, { new: true})
+        );
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
+
+//Edit Bookmarks
+app.put('/bookmakrs/edit/:id', async (req, res)=>{
+    try {
+        res.json(Bookmark.findByIdAndUpdate(req.params.id, req.body, { }))
+    } catch {
+        res.status (400).json(error)
+    }
+});
+
+//Destroy Bookmarks
+app.delete('/bookmark/:id', async (req, res)=>{
+    try {
+        res.json(await Bookmark.findByIdAndRemove(req.params.id));
+    } catch (error){
+        res.status(400).json(error)
+    }
+});
+
+//Show Bookmarks
+app.get('/bookmark/:id', async (req, res)=>{
+    try {
+        res.json(await Bookmark.find(req.params.id));
+    } catch (error) {
+        res.status(400).json(error)
+    }
+});
 
 
 
